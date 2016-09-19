03c43a87d29267d40ef0237a7bd9cc078374a80b
ferrafmz <martin.ferrarese@gmail.com>
2016-09-19 19:50:32 -0300

    Update TestPolinomio.java

--- src/testTP3/TestPolinomio.java
+++ src/testTP3/TestPolinomio.java
@@ -9,12 +9,51 @@ import polinomio.Polinomio;
 
 public class TestPolinomio {
 
-	@Test
+	/*@Test
 	public void evaluarMSucesivas() throws FileNotFoundException {
 		Polinomio polinomio = new Polinomio("LotesDePrueba\\Polinomio\\Entrada\\PolinomioEvaluarMSucesivas.in");
 		double resultadoEsperado = 238789;
 		double X = 2;
 		Assert.assertEquals(resultadoEsperado, polinomio.evaluarMSucesivas(X), 0);
+	
+	}*/
+
+	
+	/*@Test
+	public void evaluarRecursiva() throws FileNotFoundException {
+		Polinomio polinomio = new Polinomio("LotesDePrueba\\Polinomio\\Entrada\\PolinomioEvaluarMSucesivas.in");
+		double resultadoEsperado = 238789;
+		double X = 2;
+		Assert.assertEquals(resultadoEsperado, polinomio.evaluarRecursiva(X), 0);
+	
+	}*/
+	
+	/*@Test
+	public void evaluarProgDinamica() throws FileNotFoundException {
+		Polinomio polinomio = new Polinomio("LotesDePrueba\\Polinomio\\Entrada\\PolinomioEvaluarMSucesivas.in");
+		double resultadoEsperado = 238789;
+		double X = 2;
+		Assert.assertEquals(resultadoEsperado, polinomio.evaluarProgDinamica(X), 0);
+	
+	}*/
+	  /*
+	@Test
+	public void evaluarPow() throws FileNotFoundException {
+		Polinomio polinomio = new Polinomio("LotesDePrueba\\Polinomio\\Entrada\\PolinomioEvaluarMSucesivas.in");
+		double resultadoEsperado = 238789;
+		double X = 2;
+		Assert.assertEquals(resultadoEsperado, polinomio.evaluarPow(X), 0);
+	
+	}*/
+	
+
+	@Test
+	public void evaluarHorner() throws FileNotFoundException {
+		Polinomio polinomio = new Polinomio("LotesDePrueba\\Polinomio\\Entrada\\PolinomioEvaluarMSucesivas.in");
+		double resultadoEsperado = 238789;
+		double X = 2;
+		Assert.assertEquals(resultadoEsperado, polinomio.evaluarHorner(X), 0);
+	
 	}
 
 }
